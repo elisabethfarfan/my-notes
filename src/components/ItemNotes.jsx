101153057e9e2e9b4eccf350205a0674e9b406b7
elisabethfarfan <elisabethfarfan@gmail.com>
2023-01-14 21:55:06 -0500

    item completed decoration css

--- src/components/ItemNotes.jsx
+++ src/components/ItemNotes.jsx
@@ -5,7 +5,7 @@ export const ItemNotes = (props) => {
   return (
     <li className='liNote'>
         <span className={`checkNote  ${props.completed && 'Icon-check--active'}`}>✔</span>
-        <p className='textNote'>{props.text}</p>
+        <p className={`textNote ${props.completed && 'NoteItem-p--complete'}`}>{props.text}</p>
         <span className={`deleteNote ${props.completed && 'Icon-delete'}`}>X</span>
     </li>
   )
