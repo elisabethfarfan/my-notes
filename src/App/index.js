fd2a3fac505dd23d5ce4de341df7d8408bc65a9a
elisabethfarfan <elisabethfarfan@gmail.com>
2023-03-26 00:31:58 -0500

    Persistencia de datos con localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -3,14 +3,25 @@ import './App.css';
 
 import {AppUI} from './AppUI'
 
-const notesDefault = [
-  {text:'primera nota', completed:false},
-  {text:'segunda nota', completed:true},
-  {text:'tercera nota', completed:false},
+// const notesDefault = [
+//   {text:'primera nota', completed:false},
+//   {text:'segunda nota', completed:true},
+//   {text:'tercera nota', completed:false},
   
-]
+// ]
 function App() {
-  const [notes, setNotes] = React.useState(notesDefault);
+
+  const localStorageNotes = localStorage.getItem('NOTES_V1')
+  let parseNotes;
+
+  if(!localStorageNotes){
+    localStorage.setItem('NOTES_V1', JSON.stringify([]))
+    parseNotes = []
+  }else{
+    parseNotes = JSON.parse(localStorageNotes)
+  }
+
+  const [notes, setNotes] = React.useState(parseNotes);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedNotes = notes.filter((note) => !!note.completed).length
@@ -27,11 +38,17 @@ function App() {
      } )
   }
 
+  const saveNotes = (newNotes) =>{
+    const stringifiedNotes = JSON.stringify(newNotes)
+    localStorage.setItem('NOTES_V1', stringifiedNotes)
+    setNotes(newNotes)
+  }
+
   const completeNote = (text) =>{
     const indexText = notes.findIndex(note => note.text  === text)
     const newNotes = [...notes];
     newNotes[indexText].completed = true;
-    setNotes(newNotes)
+    saveNotes(newNotes)
 
   }
 
@@ -39,7 +56,7 @@ function App() {
     const indexText = notes.findIndex(note => note.text  === text)
     const newNotes = [...notes];
     newNotes.splice(indexText, 1)
-    setNotes(newNotes)
+    saveNotes(newNotes)
 
   }
 
